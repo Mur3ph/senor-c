d5ba53801e2b70d8c26f1a978e6a3ec1b94250fc
Mur3ph <paulmurphymainstreet@gmail.com>
2018-10-15 22:41:14 +0100

    initial commit

---
+++ starter-c/Main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void mainx()
+{
+	printf("I am alive!  Beware.\n");
+	getchar();
+	return 0;
+}
---
+++ starter-c/Multiplication.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int multiple(int x, int y);
+
+void main()
+{
+	int x;
+	int y;
+
+	printf("Please input two numbers to be multiplied: ");
+	scanf_s("%d", &x);
+	scanf_s("%d", &y);
+	printf("The product of your two numbers is %d\n", multiple(x, y));
+	getchar();
+	getchar();
+}
+
+int multiple(int x, int y)
+{
+	return x * y;
+}
